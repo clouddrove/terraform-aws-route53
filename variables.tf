bb0ba518b676e8aedcb666d96c202201ba5cd246
Anmol Nagpal <anmol@clouddrove.com>
2022-01-05 18:41:05 +0530

    add tag for label

--- variables.tf
+++ variables.tf
@@ -19,13 +19,13 @@ variable "environment" {
 }
 
 variable "label_order" {
-  type        = list
+  type        = list(any)
   default     = []
   description = "Label order, e.g. `name`,`application`."
 }
 
 variable "attributes" {
-  type        = list
+  type        = list(any)
   default     = []
   description = "Additional attributes (e.g. `1`)."
 }
@@ -43,7 +43,7 @@ variable "delimiter" {
 }
 
 variable "tags" {
-  type        = map
+  type        = map(any)
   default     = {}
   description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)."
 }
@@ -128,44 +128,44 @@ variable "vpc_id" {
 }
 
 variable "types" {
-  type        = list
+  type        = list(any)
   default     = []
   description = "The record type. Valid values are A, AAAA, CAA, CNAME, MX, NAPTR, NS, PTR, SOA, SPF, SRV and TXT. "
 }
 
 variable "ttls" {
-  type        = list
+  type        = list(any)
   default     = []
   description = "(Required for non-alias records) The TTL of the record."
 }
 
 variable "names" {
-  type        = list
+  type        = list(any)
   default     = []
   description = "The name of the record."
 }
 
 variable "values" {
-  type        = list
+  type        = list(any)
   default     = []
   description = "(Required for non-alias records) A string list of records. To specify a single record value longer than 255 characters such as a TXT record for DKIM, add \"\" inside the Terraform configuration string (e.g. \"first255characters\"\"morecharacters\")."
 }
 
 variable "set_identifiers" {
-  type        = list
+  type        = list(any)
   default     = []
   description = "Unique identifier to differentiate records with routing policies from one another. Required if using failover, geolocation, latency, or weighted routing policies documented below."
 }
 
 variable "health_check_ids" {
-  type        = list
+  type        = list(any)
   default     = []
   description = "The health check the record should be associated with."
 }
 
 variable "alias" {
-  type        = map
-  default     = {"names"=[],"zone_ids"=[],"evaluate_target_healths"=[]}
+  type        = map(any)
+  default     = { "names" = [], "zone_ids" = [], "evaluate_target_healths" = [] }
   description = "An alias block. Conflicts with ttl & records. Alias record documented below."
 }
 
@@ -190,13 +190,13 @@ variable "weighted_routing_policies" {
 }
 
 variable "multivalue_answer_routing_policies" {
-  type        = list
+  type        = list(any)
   default     = []
   description = "Set to true to indicate a multivalue answer routing policy. Conflicts with any other routing policy."
 }
 
 variable "allow_overwrites" {
-  type        = list
+  type        = list(any)
   default     = []
   description = "Allow creation of this record in Terraform to overwrite an existing record, if any. This does not affect the ability to update the record in Terraform and does not prevent other resources within Terraform or manual Route 53 changes outside Terraform from overwriting this record. false by default. This configuration is not recommended for most environments."
 }
